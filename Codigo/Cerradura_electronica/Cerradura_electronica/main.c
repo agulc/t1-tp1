97f524a0ef2fb43d6e80a095a57a55cf1dc69413
Agustin Cao <alcaolpg@gmail.com>
2021-05-04 12:35:55 -0300

    Base desarrollo - codigo
    Creados archivos proyecto Atmel Studio 7

---
+++ Codigo/Cerradura_electronica/Cerradura_electronica/main.c
@@ -0,0 +1,18 @@
+/*
+ * Cerradura_electronica.c
+ *
+ * Created: 04/05/2021 12:30:07
+ * Author : Agustín
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
