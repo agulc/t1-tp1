2ce644b2d9fdcdaa83ca0963508cb77c34489be5
soyezequiel <alcayaga147147@alu.ing.unlp.edu.ar>
2021-05-07 15:36:38 -0300

    se mueve el código de testeo
    se saca el código de testeo del teclado del main para pasar a estar en un modulo

--- Codigo/Cerradura_electronica/Cerradura_electronica/main.c
+++ Codigo/Cerradura_electronica/Cerradura_electronica/main.c
@@ -104,7 +104,6 @@ uint8_t KEYPAD_scan(uint8_t *key)
 	}
 	return 0;
 }
-
 uint8_t KEYPAD_Update (uint8_t *pkey)
 {
 	static uint8_t Old_key,Key, Last_valid_key=0xFF; // no hay tecla presionada
@@ -124,23 +123,26 @@ uint8_t KEYPAD_Update (uint8_t *pkey)
 	return 0;
 }
 
+//este test de funcionamiento del teclado es para probarlo con el proteus, con un keypad-smallcalc modificado y con un LCD LM016L
+void testeo_del_teclado()
+{
+		LCDinit();
+		LCDGotoXY(0,0);
+		while (1)
+		{
+			uint8_t tecla=0;
+			//programa de testeo del KEYPAD_scan
+			if(KEYPAD_Update(&tecla)){
+				LCDsendChar(tecla);
+			}
+			_delay_ms(10);
+		}
+}
 
 int main(void)
 {
-	LCDinit();
-	LCDGotoXY(0,0);
-	while (1)
-	{
-		uint8_t tecla=0;
-		//programa de testeo del KEYPAD_scan
-		if(KEYPAD_Update(&tecla)){
-			LCDsendChar(tecla);
-		}
-		_delay_ms(10);
-	}
+	testeo_del_teclado();
 	return 0;
-
-
 }
 
 
