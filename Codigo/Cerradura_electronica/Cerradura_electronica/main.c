f837de932bfb4fc09d275bdf2b2b88e86d053d88
Markos <marmoscoso1@gmail.com>
2021-05-08 17:30:04 -0300

    funciones para imprimir

--- Codigo/Cerradura_electronica/Cerradura_electronica/main.c
+++ Codigo/Cerradura_electronica/Cerradura_electronica/main.c
@@ -11,14 +11,7 @@
 #define F_CPU 8000000L
 #include <util/delay.h>
 #include <string.h>
-
-#define BLOCKED "CERRADO"
-#define OPEN "LIBERADO"
-#define DENIED "DENEGADO"
-#define PASSWORD = 0852
-enum week{CERRADO, LIBERADO, DENEGADO};
-	
-	
+uint8_t x = 16;	
 void mostrarString(const uint8_t *dato, uint8_t x, uint8_t y, uint8_t size ){ //recibe el string, la posición a ser mostrado y la longitud del string
 	uint8_t i;
 	LCDGotoXY(x,y);
@@ -26,44 +19,34 @@ void mostrarString(const uint8_t *dato, uint8_t x, uint8_t y, uint8_t size ){ //
 		LCDsendChar(dato[i]);
 	}
 }
-void muestreoEstado( enum week estado_actual ) //muestra en el display según el estado en el que esté el display
-{ 
-	if(estado_actual == CERRADO){
-		uint8_t locked[] = BLOCKED; //se crea un string que contiene la palabra CERRADO
-		mostrarString(locked,0,1, sizeof(locked)); //se muestra la palabra CERRADO en el display
-		/*muestra la hora, falta eso*/
-		//time_t t = time(NULL); //crea una variable tiempo
-		//struct tm *tm = localtime(&t); //se le da el tiempo actual
-		//uint8_t tiempo[16]; //variable para almacenar el tiempo como string
-		//strftime(tiempo, sizeof(tiempo), "%c", tm); //guarda en el string tiempo lo que hay en tm
-		//mostrarString (tiempo,0,0, sizeof(tiempo));
-	}else
-		if ( estado_actual == DENEGADO){
-			LCDclr();
-			uint8_t locked[] = DENIED;
-			mostrarString(locked,0,1, sizeof(locked));
-			_delay_ms(2000); //espera 2 segundos
-			muestreoEstado(CERRADO); //vuelve a la pantalla por defecto
-		}else{
-			LCDclr();
-			uint8_t locked[] = OPEN;
-			mostrarString(locked,0,1, sizeof(locked));
-			_delay_ms(2000); //espera 2 segundos
-			muestreoEstado(CERRADO); //vuelve a la pantalla por defecto
-		}
-			
+void mostrarArriba(const uint8_t *dato){ //recibe el string, la posición a ser mostrado y la longitud del string
+	uint8_t i;
+	LCDGotoXY(0,0);
+	for(i=0;i<x;i++){ //no se porque -2 pero así si
+		LCDsendChar(dato[i]);
+	}
+}
+void mostrarAbajo(const uint8_t *dato){ //recibe el string, la posición a ser mostrado y la longitud del string
+	uint8_t i;
+	LCDGotoXY(0,1);
+	for(i=0;i<x;i++){ //no se porque -2 pero así si
+		LCDsendChar(dato[i]);
+	}
 }
 int main(void)
 {
-	uint8_t locked[7] = BLOCKED;//{'c','e','r','r','a','d','o'}; //crea un string con la palabra cerrado
+	uint8_t locked[8] = {1+'0',6+'0',':','2','0',':','0','0'};
+	
 	LCDinit(); //se inicializa el display
+	mostrarArriba(locked);
 	LCDGotoXY(0,0);
 	//muestreoInicial();
-	mostrarString(locked,0,1, sizeof(locked));
+	//mostrarString(locked,0,1, sizeof(locked));
+	LCDcursorOnBlink();
     /* Replace with your application code */
     while (1) 
     {
-		
+			
     }
 }
 
