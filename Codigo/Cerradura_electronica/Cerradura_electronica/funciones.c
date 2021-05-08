f2fd6971ff057093fc3161ef84128f5eef968c4c
Agustin Cao <alcaolpg@gmail.com>
2021-05-08 00:02:07 -0300

    "Ingresar clave entrada" y comparación
    Implementado estado 2 "ingresar clave entrada", falta datos para display. implementado comparación de claves.

--- Codigo/Cerradura_electronica/Cerradura_electronica/funciones.c
+++ Codigo/Cerradura_electronica/Cerradura_electronica/funciones.c
@@ -1,11 +1,16 @@
 /*Este programa es la maquina de estados principal que controla la parte interactiva de la cerradura.
 Para mas información, ver la documentación del proyecto*/
 
+unsigned char pantalla_principal(uint8_t tecla);
+unsigned char ingresar_clave_entrada(uint8_t tecla);
+unsigned char comparar_claves();
+
 uint8_t clave_mascara[] = {'x','x','x','x'} ;
 uint8_t clave_ingresada[] = clave_mascara ;
 uint8_t clave_nueva[] = clave_mascara ;
 uint8_t clave_actual[] = {'0','8','5','2'} ;
-
+char cursor_clave = 0;
+char tam_clave = (char) sizeof(clave_mascara)/sizeof(clave_mascara[0]) - 1;
 
 
 void mef_funciones(unsigned char *masc_reloj, uint8_t tecla)//Analizar que devería recibir una vez establecido el modulo LCD
@@ -22,35 +27,84 @@ unsigned char pantalla_principal(uint8_t tecla)
 
     switch (tecla)
     {
-    case '0','1','2','3','4','5','6','7','8','9':
-	
-		clave_ingresada[0] = tecla;
-		proximo_estado = 2;
-        break;
-	
-	case 'A':
-	
-		proximo_estado = 8;
-		break;
+		case '0','1','2','3','4','5','6','7','8','9':
+			clave_ingresada[cursor_clave] = tecla;
+			cursor_clave++;
+			proximo_estado = 2; //Ingresar clave entrada
+			break;
 		
-	case 'B':
-	
-		proximo_estado = 9;
-		break;
-	
-	case 'C':
+		case 'A':
+			proximo_estado = 8; //Modificar hora
+			break;
 		
-		proximo_estado = 10;
-		break;
-	
-	case 'D':
-	
-		proximo_estado = 5;
-		break;
-    
-    default:
-        break;
+		case 'B':
+			proximo_estado = 9; //Modificar minutos
+			break;
+		
+		case 'C':
+			proximo_estado = 10; //Modificar segundos
+			break;
+		
+		case 'D':
+			proximo_estado = 5; //Cambiar clave: Clave actual
+			break;
+		
+		default:
+			break;
     }
 	
 	return proximo_estado;
+}
+
+unsigned char ingresar_clave_entrada(uint8_t tecla)
+{
+	//Mostrar "Ingresar clave" y  "*" por cada caracter ingresado. Se mantiene hasta haber ingresado una clave de 4 caracteres.
+	unsigned char proximo_estado = 2;
+
+	
+	//seteo lo que se tenga que mostrar en el display
+	
+	switch (tecla)
+	{
+		case '0','1','2','3','4','5','6','7','8','9':
+			clave_ingresada[cursor_clave] = tecla;
+			cursor_clave++;
+			
+			if (cursor_clave > tam_clave)
+			{
+				cursor_clave = 0;
+				proximo_estado = 4; //Denegado
+				
+				if (comparar_claves())
+				{
+					proximo_estado = 3; //Abierto
+				}
+			}
+			
+			break;
+		
+		default:
+			break;
+	}
+	
+	return proximo_estado;
+	
+}
+
+unsigned char comparar_claves()
+{
+	char comparacion = 1;
+	
+	for (char cursor = 0; cursor < tam_clave; cursor++)
+	{
+		if (clave_ingresada[cursor] != clave_actual[cursor])
+		{
+			comparacion = 0;
+			break;
+		}
+	}
+	
+	clave_ingresada = clave_mascara;
+	
+	return comparacion;
 }
