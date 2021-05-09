f3ed1dd4a4f264cf57eba9ac2c34edef1709cb68
Agustin Cao <alcaolpg@gmail.com>
2021-05-09 00:08:38 -0300

    Modulo controlador_lcd.c
    controlador_lcd.c contiene las funciones utilizadas para imprimir por nivel en el display

--- Codigo/Cerradura_electronica/Cerradura_electronica/main.c
+++ Codigo/Cerradura_electronica/Cerradura_electronica/main.c
@@ -11,28 +11,8 @@
 #define F_CPU 8000000L
 #include <util/delay.h>
 #include <string.h>
-uint8_t x = 16;	
-void mostrarString(const uint8_t *dato, uint8_t x, uint8_t y, uint8_t size ){ //recibe el string, la posición a ser mostrado y la longitud del string
-	uint8_t i;
-	LCDGotoXY(x,y);
-	for(i=0;i<size;i++){ //no se porque -2 pero así si
-		LCDsendChar(dato[i]);
-	}
-}
-void mostrarArriba(const uint8_t *dato){ //recibe el string, la posición a ser mostrado y la longitud del string
-	uint8_t i;
-	LCDGotoXY(0,0);
-	for(i=0;i<x;i++){ //no se porque -2 pero así si
-		LCDsendChar(dato[i]);
-	}
-}
-void mostrarAbajo(const uint8_t *dato){ //recibe el string, la posición a ser mostrado y la longitud del string
-	uint8_t i;
-	LCDGotoXY(0,1);
-	for(i=0;i<x;i++){ //no se porque -2 pero así si
-		LCDsendChar(dato[i]);
-	}
-}
+#include "controlador_lcd.c"
+
 int main(void)
 {
 	uint8_t locked[8] = {1+'0',6+'0',':','2','0',':','0','0'};
